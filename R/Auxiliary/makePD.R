a86941762a2844169b74d4c88b3c186df6b97c8b
Jesús N. Pinto-Ledezma <jesuspintoledezma@gmail.com>
2022-02-21 10:04:56 -0600

    add makePD

---
+++ R/Auxiliary/makePD.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(ape)
+
+makePD <- function(tree, pam, grid) { 
+  
+  gridNames <- grid$grids
+  
+  srGrid <- numeric(length = nrow(grid))
+  pdGrid <- numeric(length = nrow(grid))
+  
+  for(i in 1:nrow(grid)) { 
+    svMisc::progress(value = i, max.value = nrow(grid), progress.bar = TRUE) 
+    
+    sppSel <- pam %>% 
+      filter(grids == gridNames[i])
+    
+    treeSel <- drop.tip(tree, setdiff(tree$tip.label, sppSel$species))
+    
+    srGrid[i] <- length(sppSel$species)
+    pdGrid[i] <- sum(treeSel$edge.length)
+    
+  } 
+  
+  resPD <- data.frame(grids = gridNames, SR = srGrid, PD = pdGrid) 
+  
+  res <- full_join(resPD, grid, by = "grids")
+  
+  return(res)
+  
+}
